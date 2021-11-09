f4bd672a396fbc295bf4cfb4f97bcca819e8bf79
yorkabcd <93572146+yorkabcd@users.noreply.github.com>
2021-11-09 16:32:44 +0800

    Add files via upload

---
+++ 計算BMI/src/Variable.java
@@ -0,0 +1,9 @@
+public class Variable {
+    public static void main(String[] args) {
+        int weight = 65;
+        int height = 171;
+        int BMI = (weight / ((height/100)^2));
+        System.out.println(BMI);
+    }
+}
+
